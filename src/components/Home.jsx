c015abbd467ddbdc02584f311b88f97ca1f09bdb
meenajaswanth <mmjnk@meenajaswanths-MacBook-Air.local>
2021-10-08 21:17:24 +0530

    Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,28 +12,35 @@ export const Home = () => {
           <div className="card">
             <img src={item.image} alt="images" style={{ width: "230px" }} />
             <h1>{item.brandnName}</h1>
-            <p>
-              <strong>Rs. {item.offeredPrice}</strong>
-            </p>
             <div>{item.description}</div>
-            <button
-              onClick={() => {
-                navigate("/cart");
-                setCartitems((items) => [...items, item]);
-              }}
-              className="btn btn-outline-primary"
-            >
-              Add to Cart
-            </button>
-            <button
-              onClick={() => {
-                navigate("/wishlist");
-                setWishlist((items) => [...items, item]);
-              }}
-              className="btn btn-outline-primary"
-            >
-              Add to Wishlist
-            </button>
+            <p className="lol">
+              <strong>Rs. {item.offeredPrice}</strong>
+            </p>{" "}
+            <p className="suraj">Rs. {item.actualPrice}</p> (
+            {Math.round(
+              ((item.actualPrice - item.offeredPrice) / item.actualPrice) * 100
+            )}
+            % off)
+            <div>
+              <button
+                onClick={() => {
+                  navigate("/cart");
+                  setCartitems((items) => [...items, item]);
+                }}
+                className="btn btn-outline-primary"
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={() => {
+                  navigate("/wishlist");
+                  setWishlist((items) => [...items, item]);
+                }}
+                className="btn btn-outline-primary"
+              >
+                Add to Wishlist
+              </button>
+            </div>
           </div>
         ))}
       </div>
