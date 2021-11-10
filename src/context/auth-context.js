0677676736bf1534b3d178e7614c88790f862879
meenajaswanth <mmjnk@meenajaswanths-MacBook-Air.local>
2021-11-10 20:52:20 +0530

    Feat-Auth-context

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 export const authContext = createContext();
 
 function AuthProvider({ children }) {
-  const [login, setLogin] = useState(true);
+  const [login, setLogin] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
